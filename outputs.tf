bf51d1bd92d350f701e83d6c89a5cc5bea91efdd
Boris Schrijver <bschrijver@schubergphilis.com>
2019-11-22 22:09:50 +0100

    Add unique_id to outputs

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "arn" {
   value       = aws_iam_role.default.arn
   description = "ARN of the role"
 }
+
+output "unique_id" {
+  value       = aws_iam_role.default.unique_id
+  description = "Unique ID of the role"
+}
