163d6b6e87506b6db99ededf08b1e31c949f5056
ghawker <ghawker@schubergphilis.com>
2020-03-05 14:14:36 +0100

    arrange outputs alphabetically.

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,19 @@
-output "id" {
-  value       = aws_iam_role.default.id
-  description = "ID of the role"
-}
-
 output "arn" {
   value       = aws_iam_role.default.arn
   description = "ARN of the role"
 }
 
-output "unique_id" {
-  value       = aws_iam_role.default.unique_id
-  description = "Unique ID of the role"
+output "id" {
+  value       = aws_iam_role.default.id
+  description = "ID of the role"
 }
 
 output "name" {
   value       = aws_iam_role.default.name
   description = "The name of the role"
+}
+
+output "unique_id" {
+  value       = aws_iam_role.default.unique_id
+  description = "Unique ID of the role"
 }
